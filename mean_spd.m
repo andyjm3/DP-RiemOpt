48cb83c104133fd993e96d15127cc3c26df82569
Andi Han <68631477+andyjm3@users.noreply.github.com>
2022-11-27 18:03:27 +1100

    Update mean_spd.m

--- mean_spd.m
+++ mean_spd.m
@@ -22,7 +22,7 @@ function mean_spd()
     repeats = 20;
     ns = 1:15;
     ns = ns * 20;
-        
+    
     outgap_prgd = zeros(length(ns), repeats); 
     for in = 1:length(ns)
         n = ns(in);
@@ -30,6 +30,7 @@ function mean_spd()
         T = n;
         sigma = T*log(1/delta) * L0^2/(n^2*eps^2);
         sigma = sqrt(sigma);
+        %keyboard;
     
         
         % generate samples 
@@ -83,8 +84,9 @@ function mean_spd()
                 % sample using MHsample
                 pdf = @gpdf;
                 proprnd = @(xi) xi + randn(1,d);  
-                noise = mhsample(zeros(1,d), 1, 'pdf',pdf, 'proprnd', proprnd, 'symmetric',1, 'burnin', 10000, 'thin', 10);
+                [noise, accept] = mhsample(zeros(1,d), 1, 'pdf',pdf, 'proprnd', proprnd, 'symmetric',1, 'burnin', 10000, 'thin', 10);
                 noise = vec2mat(noise);
+                %keyboard;
 
                 rgrad = rgrad + noise;
                 W_prgd = M.exp(W_prgd, rgrad, -eta);
@@ -94,11 +96,10 @@ function mean_spd()
         end        
     end
     
-    save('mean_spd.mat', 'ns', 'outgap_prgd');
+    %save('mean_spd.mat', 'ns', 'outgap_prgd');
     %}
     
-    %% baseline
-    
+    %% baseline FM-LAP
     ns = 1:15;
     ns = ns * 20; 
     outgap_base = zeros(length(ns), repeats); 
@@ -106,7 +107,7 @@ function mean_spd()
         n = ns(in);
         
         sigma = 2/(n*eps);
-        
+
         % generate samples 
         X = zeros(r,r,n);
         k = 1;
@@ -147,27 +148,59 @@ function mean_spd()
         end
         
     end
-    save('mean_spd_base.mat', 'ns', 'outgap_base');
+    %save('mean_spd_base.mat', 'ns', 'outgap_base');
     %}
     
     %%
-    
-    fs = 22;
-    
-    mean_prgd = mean(outgap_prgd, 2);
-    std_prgd = std(outgap_prgd, 0, 2);
-    
-    h = figure();
-    x = ns';
-    y = mean_prgd;
-    semilogy(x,y, 'color','#EDB120','LineWidth',3); hold on;
-    hold off;
-    ax1 = gca;
-    set(ax1,'FontSize', fs);
-    set(gca,'fontname','Arial');
-    legend('DP-RGD', 'fontsize', fs-2);
-    xlabel('Sample size', 'fontsize', fs);
-    ylabel('Empirical excess risk', 'fontsize', fs);
+    % K-norm gradient norm FM-KNG
+    ns = 1:15;
+    ns = ns * 20; 
+    epsilon = 0;
+    outgap_kng = zeros(length(ns), repeats); 
+    for in = 1:length(ns)
+        n = ns(in);
+        
+        sigma = 4/(n*eps);
+
+        % generate samples 
+        X = zeros(r,r,n);
+        k = 1;
+        while true
+            temp = wishrnd(eye(r)/r,r);
+            if M.dist(eye(r),temp) < tol
+                X(:,:,k) = temp;
+                k = k + 1;
+                if k > n; break; end
+            end
+        end
+        
+        
+        % find optimal solution using RGD
+        eta = 1;
+        W_rgd = M.rand();
+        for iter = 1:10
+            rgrad = 0;
+            for i = 1 : n
+                rgrad = rgrad - M.log(W_rgd, X(:, :, i));
+            end 
+            rgrad = rgrad./n;
+            normgrad = M.norm(W_rgd, rgrad);
+            fprintf('Iter [%0.4d]:\t norm rgrad %e\n', iter, normgrad);
+            if normgrad < 1e-14
+                break;
+            end
+            W_rgd = M.exp(W_rgd, rgrad, -eta);
+        end
+        % compute optimal cost
+        optfn = cost(W_rgd);
+        
+        for irep = 1:repeats
+            W_base = kng_sampler(W_rgd, sigma, 5000, 50000);
+            outgap_kng(in, irep) = abs(cost(W_base) - optfn);
+        end
+        
+    end
+    save('mean_spd_kng.mat', 'ns', 'outgap_kng');
     
     
     
@@ -215,6 +248,53 @@ function mean_spd()
             end
         end
         assert(k == d+1);
+    end
+
+    function xnext = kng_sampler(x0, sigma, burnin, maxepoch)
+        xcur = x0;
+        rgrad = 0;
+        for jj = 1 : n
+            rgrad = rgrad - M.log(xcur, X(:, :, jj));
+        end 
+        g_cur = exp(-1/sigma * M.norm(xcur, rgrad)/n);
+        
+        accept_count = 0;
+        for ii = 1: maxepoch
+            v = unifrnd(-0.5, 0.5, 3,1);
+            V = vec2mat(v);
+            xnext = M.exp(xcur, 0.1*sigma*V) + epsilon*eye(r);
+            rgrad = 0;
+            for jj = 1 : n
+                rgrad = rgrad - M.log(xnext, X(:, :, jj));
+            end 
+            g_next = exp(-1/sigma * M.norm(xnext, rgrad)/n);
+            
+            accept_prob = g_next / g_cur;
+            xrand = rand;
+            if xrand < accept_prob
+                accept_count = accept_count + 1;
+                xcur = xnext;
+                g_cur = g_next;
+            end
+            if accept_count >= burnin
+                break
+            end
+            if ii == maxepoch
+                disp('Max iteration reached!');
+            end
+            
+        end
+        
+    end
+
+    function q = kngpdf(xi)
+        Xi = vec2mat(xi);
+        Xi = expm(Xi);
+        rgrad = 0;
+        for ii = 1 : n
+            rgrad = rgrad - M.log(Xi, X(:, :, ii));
+        end 
+        q = exp(-1/sigma *  M.norm(Xi, rgrad)/n);
     end
     
     
